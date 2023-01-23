8d1de34f9d991608ea7ffa2f0732ea77c0eeb86d
skAdOOdlEs7 <grantdriddle7@gmail.com>
2023-01-23 08:56:36 -0700

    short

--- misty.py
+++ misty.py
@@ -1,10 +1,11 @@
 import requests
 
+
 class Misty:
     def __init__(self, ip):
         self.ip = ip
 
-    def post(self, command, headers, parameters):
+    def post(self, command, parameters):
         text = ""
         for i in parameters.keys():
             text += f"{i}={parameters[i]}"
@@ -13,19 +14,14 @@ class Misty:
         return requests.post(
             f"http://{self.ip}/api/{command}?{text}",
             json=parameters,
-            headers=headers
+            headers={"Content-Type": "application/json"}
         )
 
 
 Chuck = Misty("172.22.174.127")
 
 print(Chuck.post(
-    "drive", 
-{
-    "Content-Type": "application/json"
-}, 
-{
-    "linearVelocity": 100,
-    "angularVelocity": 100
-}
+    "halt",
+    {
+    }
 ))
