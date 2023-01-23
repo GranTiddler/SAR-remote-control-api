707a31b3c3190dcb419bf2a765d91c9549279f0c
skAdOOdlEs7 <grantdriddle7@gmail.com>
2023-01-23 09:30:00 -0700

    added delete and get functions, default params

--- misty.py
+++ misty.py
@@ -5,7 +5,7 @@ class Misty:
     def __init__(self, ip):
         self.ip = ip
 
-    def post(self, command, parameters):
+    def post(self, command, parameters={}):
         text = ""
         for i in parameters.keys():
             text += f"{i}={parameters[i]}"
@@ -17,14 +17,36 @@ class Misty:
             headers={"Content-Type": "application/json"}
         )
 
+    def get(self, command, parameters={}):
+        text = ""
+        for i in parameters.keys():
+            text += f"{i}={parameters[i]}"
+            text += "&"
+
+        return requests.get(
+            f"http://{self.ip}/api/{command}?{text}"
+        )
+
+    def delete(self, command, parameters={}):
+        text = ""
+        for i in parameters.keys():
+            text += f"{i}={parameters[i]}"
+            text += "&"
+
+        return requests.delete(
+            f"http://{self.ip}/api/{command}?{text}"
+        )
+
     def display_image(self, filename, alpha):
-        self.post("image/display", {"filename": filename, "alpha" : alpha})
+        return self.post("image/display", {"FileName": filename, "Alpha": alpha})
+
+    def get_image_list(self):
+        return self.get("images/list")
+
+    def get_blink_settings(self):
+        return self.get("blink/settings")
 
 
 Chuck = Misty("172.22.174.127")
 
-print(Chuck.post(
-    "halt",
-    {
-    }
-))
+print(Chuck.delete("images",{"FileName" : "diglett.png"}))
