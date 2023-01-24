7274e448fad9bfc221df649dc944f965153cc24d
shippy456 <tank_spbs77@icloud.com>
2023-01-23 17:36:59 -0700

    I did stuff

--- misty.py
+++ misty.py
@@ -19,6 +19,28 @@ class Misty:
 
     def display_image(self, filename, alpha):
         self.post("image/display", {"filename": filename, "alpha" : alpha})
+    
+    def audio(self, filename, data, immediatelyapply, overwriteexisting):
+       return self.post({"FileName": filename, "Data": data, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+
+    def images(self, filename, data, width, height, immediatelyapply, overwriteexisting):
+        return self.post({"FileName": filename, "Data": data, "Width": width, "Height": height, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+    
+    #def #video goes here :P
+
+    def led(self, red, green, blue):
+        return self.post({"red": red, "green": green, "blue": blue})
+
+    def videos_display(self, url, layer):
+        return self.post({"URL": url, "Layer": layer})
+
+    def webviews_display(self, url):
+        return self.post({"URL": url})
+
+    def pause_audio(self,):
+        return self.post({  })
+    
+    
 
 
 Chuck = Misty("172.22.174.127")
