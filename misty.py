473bacf1bcb379fbd7a43f80c3a5ca61290862a8
shippy456 <tank_spbs77@icloud.com>
2023-01-23 17:41:10 -0700

    merge error

--- misty.py
+++ misty.py
@@ -38,7 +38,33 @@ class Misty:
         )
 
     def display_image(self, filename, alpha):
-        self.post("image/display", {"filename": filename, "alpha" : alpha})
+        return self.post("image/display", {"FileName": filename, "Alpha": alpha})
+    
+    def audio(self, filename, data, immediatelyapply, overwriteexisting):
+       return self.post({"FileName": filename, "Data": data, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+
+    def images(self, filename, data, width, height, immediatelyapply, overwriteexisting):
+        return self.post({"FileName": filename, "Data": data, "Width": width, "Height": height, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+    
+    #def #video goes here :P
+
+    def led(self, red, green, blue):
+        return self.post({"red": red, "green": green, "blue": blue})
+
+    def videos_display(self, url, layer):
+        return self.post({"URL": url, "Layer": layer})
+
+    def webviews_display(self, url):
+        return self.post({"URL": url})
+
+    def pause_audio(self,):
+        return self.post({  })
+
+    def get_image_list(self):
+        return self.get("images/list")
+
+    def get_blink_settings(self):
+        return self.get("blink/settings")
 
 
 Chuck = Misty("172.22.174.127")
