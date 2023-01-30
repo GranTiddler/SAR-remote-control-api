01e6325fd0ff6b7062e2bbbb88e6937ff721241c
shippy456 <tank_spbs77@icloud.com>
2023-01-30 16:26:34 -0700

    fixed post addresses and clarified definitions

--- misty.py
+++ misty.py
@@ -40,25 +40,25 @@ class Misty:
     def display_image(self, filename, alpha):
         return self.post("image/display", {"FileName": filename, "Alpha": alpha})
     
-    def audio(self, filename, data, immediatelyapply, overwriteexisting):
-       return self.post({"FileName": filename, "Data": data, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+    def save_audio(self, filename, data, immediatelyapply, overwriteexisting):
+       return self.post("audio", {"FileName": filename, "Data": data, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
 
-    def images(self, filename, data, width, height, immediatelyapply, overwriteexisting):
-        return self.post({"FileName": filename, "Data": data, "Width": width, "Height": height, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
+    def save_images(self, filename, data, width, height, immediatelyapply, overwriteexisting):
+        return self.post("images", {"FileName": filename, "Data": data, "Width": width, "Height": height, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
     
-    #def #video goes here :P
+    #def #save_video goes here :P
 
-    def led(self, red, green, blue):
-        return self.post({"red": red, "green": green, "blue": blue})
+    def change_led(self, red, green, blue):
+        return self.post("led", {"red": red, "green": green, "blue": blue})
 
     def videos_display(self, url, layer):
-        return self.post({"URL": url, "Layer": layer})
+        return self.post("videos/display", {"URL": url, "Layer": layer})
 
     def webviews_display(self, url):
-        return self.post({"URL": url})
+        return self.post("webviews/display", {"URL": url})
 
     def pause_audio(self,):
-        return self.post({  })
+        return self.post("audio/pause", {  })
 
     def get_image_list(self):
         return self.get("images/list")
@@ -68,6 +68,15 @@ class Misty:
     
     def play_audio(self, filename):
         return self.post("audio/play", {"FileName": filename})
+
+    def trigger_skills_event(self, skill, eventname, payload, customkey, anotherkey, source):
+        return self.post("skills/event", {"Skill": skill, "EventName": eventname, "Payload": payload, "CustomKey": customkey, "AnotherKey": anotherkey, "Source": source})
+    
+    def set_blink(blink):
+        return self.post("blink", {"Blink": blink})
+    
+    def set_blink_settings(self, blinkimages, openeyeminms, openeyemaxms, closedeyeminms, closedeyemaxms, reverttodefault):
+        return self.post("blink/settings", {"BlinkImages": blinkimages, "OpenEyeMinMs": openeyeminms, "OpenEyeMaxMs": openeyemaxms, "ClosedEyeMinMs": closedeyeminms, "ClosedEyeMaxMs": closedeyemaxms, "RevertToDefault": reverttodefault})
 
 
 Chuck = Misty("172.22.174.127")
