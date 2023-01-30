edba90e238525070df315f3bf447eb3855f1ac73
skAdOOdlEs7 <grantdriddle7@gmail.com>
2023-01-30 16:13:21 -0700

    I forgot what I did here

--- misty.py
+++ misty.py
@@ -39,6 +39,15 @@ class Misty:
 
     def display_image(self, filename, alpha):
         return self.post("image/display", {"FileName": filename, "Alpha": alpha})
+
+    def videos_display(self, url, layer):
+        return self.post({"URL": url, "Layer": layer})
+
+    def get_image_list(self):
+        return self.get("images/list")
+
+    def get_blink_settings(self):
+        return self.get("blink/settings")
     
     def audio(self, filename, data, immediatelyapply, overwriteexisting):
        return self.post({"FileName": filename, "Data": data, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
@@ -46,27 +55,18 @@ class Misty:
     def images(self, filename, data, width, height, immediatelyapply, overwriteexisting):
         return self.post({"FileName": filename, "Data": data, "Width": width, "Height": height, "ImmediatelyApply": immediatelyapply, "OverwriteExisting": overwriteexisting})
     
-    #def #video goes here :P
+    #def video goes here :P
 
-    def led(self, red, green, blue):
-        return self.post({"red": red, "green": green, "blue": blue})
-
-    def videos_display(self, url, layer):
-        return self.post({"URL": url, "Layer": layer})
+    def change_led(self, red, green, blue):
+        return self.post("led", {"red": red, "green": green, "blue": blue})
 
     def webviews_display(self, url):
         return self.post({"URL": url})
 
-    def pause_audio(self,):
+    def pause_audio(self):
         return self.post({  })
-
-    def get_image_list(self):
-        return self.get("images/list")
-
-    def get_blink_settings(self):
-        return self.get("blink/settings")
 
 
 Chuck = Misty("172.22.174.127")
 
-print(Chuck.get_image_list().json())
+
